923b28378c41bc74d4ac200c927d2174c9565b1d
unknown <mattgvo@gmail.com>
2019-07-30 17:08:54 -0600

    added redux

--- server/index.js
+++ server/index.js
@@ -10,15 +10,14 @@ const PORT = SERVER_PORT || 4009
 app.use(express.json())
 
 massive(CONNECTION_STRING).then( db =>
-    app.set('db',db)
+    app.set('db',db);
     console.log('Server connected to DB')
-).catch(err => (
+).catch(err => {
     console.log('Error connecting to DB',err)
-))
+});
 
 app.get('/api/inventory', inventoryCtrl.getInventory)
 app.post('/api/inventory', inventoryCtrl.addInventory)
 app.delete('/api/inventory', inventoryCtrl.sellInventory)
-
 
 app.listen(PORT, () => console.log(`${PORT} slurpees, stat!`))
