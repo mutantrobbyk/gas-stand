0161ad77feb12e50d0572d6a0b985dc3af0710e0
unknown <mattgvo@gmail.com>
2019-07-30 16:40:20 -0600

    added massive connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const massive = require('massive')
 const app = express()
 require('dotenv').config()
 
-const { SERVER_PORT } = process.env
+const { SERVER_PORT, CONNECTION_STRING } = process.env
 const PORT = SERVER_PORT || 4009
 
 app.use(express.json())
+
+massive(CONNECTION_STRING).then( db =>
+    app.set('db',db)
+    console.log('Server connected to DB')
+).catch(err => (
+    console.log('Error connecting to DB',err)
+))
 
 
 app.listen(PORT, () => console.log(`${PORT} slurpees, stat!`))
